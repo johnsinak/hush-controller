db0a8d0a4da9a105f564aac9ede8cf1862e9f484
sinku <sininoir@gmail.com>
2023-12-06 21:59:54 -0500

    Add the base client manager with missing logic

---
+++ src/client_manager.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from DAO import DAO
+from models import ClientModel
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    dao = DAO()
+    client_ip = request.remote_addr
+
+    client = dao.find_client(client_ip)
+    if not client:
+        print('did not find, doing a search')
+        new_client = ClientModel(client_ip, datetime.now(), 1)
+        dao.add_client(new_client)
+        client = dao.find_client(client_ip)
+
+    return f'Client IP: {client_ip}\nResult: {client.firt_request}'
+
+def client_manager_server():
+    app.run(debug=True, host="0.0.0.0", port=5001)
