738df0eb3907185fde5a8a91f419cec4b573f1dd
sinku <sininoir@gmail.com>
2023-12-06 22:02:23 -0500

    Update program entrypoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
+from client_manager import client_manager_server
 from DAO import DAO
+from models import *
+from poller import *
+from settings import *
 
 if __name__ == "__main__":
-    dao = DAO()
+    poller = Poller(INSTANCE_MANAGER_ADDRESS)
+    poller_thread = PollerThread(poller)
+    poller_thread.start()
+
+    client_manager_server()
+    
