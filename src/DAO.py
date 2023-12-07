0bd357fea59d4378d63823404e881224274b0b1e
sinku <sininoir@gmail.com>
2023-12-06 21:59:43 -0500

    Add new features to DAO

--- src/DAO.py
+++ src/DAO.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import Error
 from settings import DATABASE_PATH
 from queries import *
+from models import *
 
 class DAO:
     def __init__(self, database_path=None) -> None:
@@ -25,7 +26,15 @@ class DAO:
         try:
             cursor.execute(query)
             self.connection.commit()
-            print("Query executed successfully")
+        except Error as e:
+            print(f"The error '{e}' occurred")
+    
+    def __execute_search__(self, query):
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(query)
+            results = cursor.fetchall()
+            return results
         except Error as e:
             print(f"The error '{e}' occurred")
 
@@ -34,10 +43,19 @@ class DAO:
         self.__execute_query__(CLIENTS_TABLE_CREATION_QUERY)
         self.__execute_query__(CONNECTIONS_TABLE_CREATION_QUERY)
 
-    def add_proxy(self, proxy):
-        #TODO:
+    def add_proxy(self, proxy:ProxyModel):
+        self.__execute_query__(PROXIES_INSERT_VALUES_QUERY.format(proxy.url, proxy.created_at, proxy.utility, proxy.connected_users, proxy.avg_throughput))
+
+    def add_client(self, client:ClientModel):
+        self.__execute_query__(CLIENTS_INSERT_VALUES_QUERY.format(client.ip, client.firt_request, client.request_count))
         pass
 
-    def add_client(self, client):
-        #TODO:
+    def find_client(self, ip) -> ClientModel:
+        results = self.__execute_search__(CLIENTS_SEARCH_VALUES.format(ip))
+        if results:
+            return ClientModel.from_list(results[0])
+        else:
+            return None
+
+    def add_connection(self, proxy:ProxyModel, client:ClientModel):
         pass
