29b05bfda7476093ae4ee812ae6ac26bf93cd2d6
sinku <sininoir@gmail.com>
2023-12-18 01:17:49 -0500

    Clean the getnew view

--- controller/assignments/views.py
+++ controller/assignments/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.views import APIView
 from rest_framework.serializers import ValidationError
 from rest_framework.response import Response
@@ -7,7 +8,9 @@ from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser
-from django.contrib.auth.models import AnonymousUser
+
+from assignments.models import Client
+
 
 # Create your views here.
 
@@ -17,7 +20,14 @@ class AssignmentView(APIView):
     """
 
     def get(self, request: Request):
-        # if not request.user.is_staff:
-        #     return Response(status=401, data="you don't have permission")
-        
-        return Response(data=f"you are2 {request.META['REMOTE_ADDR']} - {request.META['HTTP_USER_AGENT']} - {request.META['REMOTE_HOST']}", status=status.HTTP_200_OK)
+        user_ip = request.META.get('REMOTE_ADDR', None)
+        if not user_ip:
+            return Response(data=f"No Ip reported.", status=status.HTTP_400_BAD_REQUEST)
+
+        user_device = request.META.get('HTTP_USER_AGENT', 'N/A')
+        try:
+            client = Client.objects.get(ip=user_ip)
+        except ObjectDoesNotExist:
+            client = Client.objects.create(ip=user_ip, user_agent=user_device)
+
+        # TODO: create an assignment
