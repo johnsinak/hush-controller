a9c40feed3b9a7ab5f9f8961cb828aa38022d46d
sinku <sininoir@gmail.com>
2023-12-26 18:52:38 -0500

    Add Enem19 implementations

--- controller/assignments/models.py
+++ controller/assignments/models.py
@@ -4,25 +4,64 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.models import (
     PermissionsMixin,
 )
+import requests
+
+
+def get_ip_location(ip: str):
+    base_url = f"http://ipapi.co/{ip}/json/"
+    response = requests.get(base_url)
+    location = response.json()
+    return location["latitude"], location["longitude"]
+
+
+class ProxyManager(models.Manager):
+    def create(self, **kwargs):
+        proxy_ip = kwargs.get('ip', None)
+        if proxy_ip is not None:
+            latitude, longitude = get_ip_location(proxy_ip)
+            kwargs['latitude'] = latitude
+            kwargs['longitude'] = longitude
+        instance = super().create(**kwargs)
+
+        return instance
 
 
 class Proxy(models.Model):
     url = models.CharField(max_length=100, null=False)
-    description = models.CharField(max_length=255, null=True, blank=True)
-    # summon_date = models.DateTimeField(auto_now=False, null=False)
+    ip = models.CharField(max_length=30, null=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    # verified_attenders = models.ManyToManyField(
-    #     User, related_name="events_attending", blank=True
-    # )
-    is_blocked = models.BooleanField(default=True)
+    # TODO: fix this
+    capacity = models.IntegerField(default=40)
+    is_blocked = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    objects = ProxyManager()
 
     def __str__(self):
         return str(self.url)
 
 
+class ClientManager(models.Manager):
+    def create(self, **kwargs):
+        user_ip = kwargs.get('ip', None)
+        if user_ip is not None:
+            latitude, longitude = get_ip_location(user_ip)
+            kwargs['latitude'] = latitude
+            kwargs['longitude'] = longitude
+        instance = super().create(**kwargs)
+
+        return instance
+
+
 class Client(models.Model):
     ip = models.CharField(max_length=30, null=False, unique=True, primary_key=True)
     user_agent = models.CharField(max_length=255, null=True, blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    objects = ClientManager()
 
 
 class ProxyReport(models.Model):
@@ -39,8 +78,7 @@ class ProxyReport(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     utility = models.FloatField()
     throughput = models.FloatField()
-    # TODO: ask about this
-    connected_users = models.ManyToManyField(
+    connected_clients = models.ManyToManyField(
         Client, related_name="proxies_connected", blank=True
     )
 
--- controller/assignments/views.py
+++ controller/assignments/views.py
@@ -8,11 +8,12 @@ from rest_framework.request import Request
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser
+from random import randint
+from geopy.distance import geodesic
 
-from assignments.models import Client
+from assignments.models import *
 
 
-# Create your views here.
 
 class AssignmentView(APIView):
     """
@@ -29,5 +30,38 @@ class AssignmentView(APIView):
             client = Client.objects.get(ip=user_ip)
         except ObjectDoesNotExist:
             client = Client.objects.create(ip=user_ip, user_agent=user_device)
+            # TODO: provide with random proxy and done. discuss this
+            active_proxies = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all()
+            chosen_proxy = active_proxies[randint(0,len(active_proxies) - 1)]
+            Assignment.objects.create(proxy=chosen_proxy, client=client)
+            return Response(data=f"{chosen_proxy.url}", status=status.HTTP_200_OK)
 
-        # TODO: create an assignment
+        # ################ Enem19 implementation ################
+
+        # #TODO: Do we have to block users in some way? that seems odd!
+        active_proxies = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all()
+        client_assignments = Assignment.objects.filter(client=client)
+        known_blocked_proxies_for_client = client_assignments.values('proxy').filter(is_blocked=True)
+        
+        blocked_proxy_usage = 0
+        for proxy in known_blocked_proxies_for_client:
+            if not ProxyReport.objects.filter(proxy=proxy).values('clients').contains(client):
+                blocked_proxy_usage += 1
+
+        number_of_blocked_proxies_that_a_user_knows = known_blocked_proxies_for_client.count()
+        number_of_requests_for_new_proxies = client_assignments.count()
+        proxy_utilization = ProxyReport.objects.filter(connected_clients__value=client).count()
+
+        utility_values_for_client = []
+
+        #TODO:
+        alpha1, alpha2, alpha3, alpha4, alpha5 = 1,1,1,1,1
+        some_cap_value = 50
+        for proxy in active_proxies:
+            distance = 0
+            proxy_utility = alpha1 * min(proxy_utilization, some_cap_value) - \
+                            alpha2 * number_of_requests_for_new_proxies - \
+                            alpha3 * blocked_proxy_usage - \
+                            alpha4 * number_of_blocked_proxies_that_a_user_knows - \
+                            alpha5 * distance
+
