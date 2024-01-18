d8f1055d4d96c9f6a43a1c77d5a0839f75a85280
sinku <sininoir@gmail.com>
2024-01-18 10:04:48 -0500

    Add the report reading logic

--- controller/assignments/services/poller_threads.py
+++ controller/assignments/services/poller_threads.py
@@ -1,8 +1,9 @@
 import threading
 from time import sleep
 import socket
+import json
 
-from assignments.models import Proxy, ProxyReport
+from assignments.models import Proxy, ProxyReport, Client
 
 
 class PollerThread(threading.Thread):
@@ -25,10 +26,14 @@ class PollerThread(threading.Thread):
                     client_socket.send(message.encode('utf-8'))
                     data = client_socket.recv(8192)
                     client_socket.close()
-
-                    # do the shit
-                    #TODO:
-                    ProxyReport.objects.create(proxy=proxy, utility = ,throughput= , connected_clients = ))
+                    
+                    report_dict = json.loads(data.decode())
+                    report = ProxyReport.objects.create(proxy=proxy, utility=report['utility'], throughput=report['throughput'])
+                    for client_ip in report_dict['connected_clients']:
+                        if Client.objects.filter(ip=client_ip).count() != 0:
+                            client = Client.objects.get(ip=client_ip)
+                            report.connected_clients.add(client)
+                    report.save()
                     
         except Exception as e:
             print('ERROR: a fatal error has happened')
