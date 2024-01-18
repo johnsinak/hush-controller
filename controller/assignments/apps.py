491c7b8e44648f1a3e10946f2102b08d76b171d2
sinku <sininoir@gmail.com>
2024-01-17 23:19:08 -0500

    Add the new startup function

--- controller/assignments/apps.py
+++ controller/assignments/apps.py
@@ -1,6 +1,13 @@
+import os
 from django.apps import AppConfig
+from assignments.services import startup
 
 
 class AssignmentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "assignments"
+
+    def ready(self):
+        # Your function to run on startup
+        if os.environ.get('RUN_MAIN'):
+            startup.startup()
