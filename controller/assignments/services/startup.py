48f82798cadd8cb5c3fbfc20ca36e1d1ba719c64
sinku <sininoir@gmail.com>
2024-02-05 01:41:43 -0500

    Add new configs

--- controller/assignments/services/startup.py
+++ controller/assignments/services/startup.py
@@ -56,7 +56,7 @@ def setup_test_db(test_config):
 def test_startup():
         from assignments.services import test_threads
         TEST_SIZE_C_P = [(100,10), (500,50), (1000,100), (2000,200), (5000,50)]
-        test_config = TEST_SIZE_C_P[3]
+        test_config = TEST_SIZE_C_P[4]
         print(f'going with {test_config[0]}c{test_config[1]}p')
         setup_test_db(test_config)
 
