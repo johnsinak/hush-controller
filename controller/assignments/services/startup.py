2462b8694b797b98ccdd9bdd11d1f8485ff6cf8b
sinku <sininoir@gmail.com>
2024-02-04 22:55:48 -0500

    Add the new startup func

--- controller/assignments/services/startup.py
+++ controller/assignments/services/startup.py
@@ -1,6 +1,8 @@
-from assignments.models import Proxy
+from assignments.models import *
 from assignments.services import poller_threads
+from assignments.services import test_threads
 import requests
+from time import time
 
 def startup():
     if Proxy.objects.all().count() != 0:
@@ -25,3 +27,36 @@ def startup():
     poller_thread.start()
 
     #TODO: Add a system for migrations and stuff, since we don't have the push for the current test
+
+def id_to_nums(id):
+    num1 = id // 250
+    num2 = id % 250
+    return num1, num2
+
+
+def setup_test_db(test_config):
+    MIGRATION_COUNT = 10
+    client_count = test_config[0]
+    proxy_count = test_config[1]
+    clinet_per_proxy = client_count // proxy_count
+    # fake_proxy_ip_base = '1.-.-.0' through '1.-.-.10'
+    # fake_client_ip_base = '2.-.-.1' through '1.-.-.client_per_proxy'
+
+    for i in range(proxy_count):
+        num1, num2 = id_to_nums(i)
+        main_proxy = Proxy.objects.create(ip=f'1.{num1}.{num2}.{0}')
+        for i in range(MIGRATION_COUNT):
+            Proxy.objects.create(ip=f'1.{num1}.{num2}.{i+1}')
+        for i in range(clinet_per_proxy):
+            client = Client.objects.create(ip=f'2.{num1}.{num2}.{i+1}')
+            Assignment.objects.create(proxy=main_proxy, client=client)
+
+
+def test_startup():
+    TEST_SIZE_C_P = [(100,10), (500,50), (1000,100), (2000,200), (5000,50)]
+    test_config = (100,10)
+    setup_test_db(test_config)
+
+    poster_thread = test_threads.UpdatePosterThread()
+    poster_thread.start()
+
