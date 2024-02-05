57b386c6e4014c3b4c110a812e59cfa1295cad93
sinku <sininoir@gmail.com>
2024-02-05 01:37:05 -0500

    Add new configs

--- controller/assignments/services/test_threads.py
+++ controller/assignments/services/test_threads.py
@@ -13,7 +13,9 @@ class UpdatePosterThread(threading.Thread):
     def run(self):
         print(f"==== test migration sender running...")
         start_time = time()
-        TESTING_MIGRATION_TIMES = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        TESTING_MIGRATION_TIMES = []
+        for i in range(10):
+            TESTING_MIGRATION_TIMES.append(i*20 + 10)
         counters = [0] * len(TESTING_MIGRATION_TIMES)
         is_done = 0
         while True:
@@ -22,6 +24,7 @@ class UpdatePosterThread(threading.Thread):
             for i in range(len(TESTING_MIGRATION_TIMES) - 1, -1, -1):
                 if TESTING_MIGRATION_TIMES[i] < right_now and counters[i] == 0:
                     # TESTING_MIGRATION_DESTS
+                    print(f'time is: {TESTING_MIGRATION_TIMES[i]}')
                     print(f'sending to {i} to migrate to {i+1}')
                     url = f'http://3.91.73.130:8000/assignments/postupdate'
                     data = {"source_id": i, "proxy_len": self.test_size}
