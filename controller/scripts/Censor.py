c140c920cbab743fd82eea90d2a3cdc818cd5e3a
sinku <sininoir@gmail.com>
2024-02-06 23:20:53 -0500

    Add an empty run

--- controller/scripts/Censor.py
+++ controller/scripts/Censor.py
@@ -3,9 +3,15 @@
 class AggresiveCensor():
     def __init__(self) -> None:
         self.agents = []
+    
+    def run():
+        pass
 
 
 class OptimalCensor():
     def __init__(self) -> None:
         self.agents = []
+    
+    def run():
+        pass
     
