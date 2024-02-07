f3ac258915ebc0f88c49fa9626fde6795f3a8279
sinku <sininoir@gmail.com>
2024-02-07 09:53:19 -0500

    Add the plan for the tests

--- controller/scripts/run_simulation.py
+++ controller/scripts/run_simulation.py
@@ -1,32 +1,38 @@
-############ BASICS ############
-SIMULATION_DURATION = 3 * 365 * 24 # 3 years
-BIRTH_PERIOD = 1 * 365 * 24 # 1 year?
-FLEET_SIZE = 10
-CLIENT_UTILITY_THRESHOLD = 0.4
-
-############ RATES ############
-# for our reference: TIME_UNIT = 1 hour
-NEW_USER_RATE_INTERVAL = 1 # 1 user every 1 hour
-NEW_USER_COUNT = 1
-
-NEW_PROXY_INTERVAL = 30 # 1 every 30 hours
-NEW_PROXY_COUNT = 1
-
-REJUVINATION_RATE = 1 # rejuvinations are made every this many time units 
-CENSORING_AGENTS_TO_ALL_CLIENTS = 0.05 # can be 0.05, 0.1, and 0.5
-CENSORING_AGENTS_TO_ALL_CLIENTS_BIRTH_PERIOD = 0.02
+from assignments.models import Client, Proxy, ProxyReport, Assignment
+from .Censor import *
+from .config_basic import *
 
 
+############ CENSOR ############
+censor = OptimalCensor()
 
 def run_simulation(birth_period:bool):
     duration = BIRTH_PERIOD if birth_period else SIMULATION_DURATION
+    global censor
+
 
     for step in range(duration):
         # Censor chooses what proxies of the ones they have to block
-        # Clients get affected
+        list_of_proxies_to_be_blocked = censor.run()
+        # TODO: mark these proxies as blocked
+        
+        # NOTE: maybe not neccesary. Clients get affected
+        #   enumerate clients that are connected to the blocked proxies and update things?
+
         # Affected clients (agent or not) request for new proxies and see if they're blocked or not.
-        # Number of disconnected users are determined
-        # IF the rejuvination rate > 1 then count blocked proxies on intervals where there are no rejuvs 
+        #   The get proxy algo gets run for every single client that was previously connected to those blocked proxies
+
+        #TODO: at some point I should decide when the agents request for new proxies
+
+        # Number of disconnected users are determined and recorded
+        #   this is just data collection
+
+        # IF the rejuvination interval > 1 then count blocked proxies on intervals where there are no rejuvs
+        #   TODO: decide whether this is done before or after clients request for new thngs
+        if REJUVINATION_INTERVAL > 1:
+            # record_blocked stuff
+            pass
+        
 
         # New proxies (if any) get created
         if step % NEW_PROXY_INTERVAL == 0:
