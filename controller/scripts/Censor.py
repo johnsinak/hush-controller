d6e3aa78edabbf26c8ba368b36b77a13167eba49
sinku <sininoir@gmail.com>
2024-02-06 23:20:12 -0500

    Add some basic censors

---
+++ controller/scripts/Censor.py
@@ -0,0 +1,11 @@
+
+
+class AggresiveCensor():
+    def __init__(self) -> None:
+        self.agents = []
+
+
+class OptimalCensor():
+    def __init__(self) -> None:
+        self.agents = []
+    
--- controller/scripts/run_simulation.py
+++ controller/scripts/run_simulation.py
@@ -6,23 +6,37 @@ CLIENT_UTILITY_THRESHOLD = 0.4
 
 ############ RATES ############
 # for our reference: TIME_UNIT = 1 hour
-NEW_USER_RATE = 1 # 1 user every hours, which is like 20 per day?
+NEW_USER_RATE_INTERVAL = 1 # 1 user every 1 hour
+NEW_USER_COUNT = 1
+
 NEW_PROXY_INTERVAL = 30 # 1 every 30 hours
+NEW_PROXY_COUNT = 1
+
 REJUVINATION_RATE = 1 # rejuvinations are made every this many time units 
 CENSORING_AGENTS_TO_ALL_CLIENTS = 0.05 # can be 0.05, 0.1, and 0.5
 CENSORING_AGENTS_TO_ALL_CLIENTS_BIRTH_PERIOD = 0.02
 
-def run_simulation():
-    for step in range(BIRTH_PERIOD):
-        pass
 
-    for step in range(SIMULATION_DURATION):
+
+def run_simulation(birth_period:bool):
+    duration = BIRTH_PERIOD if birth_period else SIMULATION_DURATION
+
+    for step in range(duration):
         # Censor chooses what proxies of the ones they have to block
         # Clients get affected
-        # New clients get added (censor agent with chance of censoring agent)
         # Affected clients (agent or not) request for new proxies and see if they're blocked or not.
         # Number of disconnected users are determined
         # IF the rejuvination rate > 1 then count blocked proxies on intervals where there are no rejuvs 
+
+        # New proxies (if any) get created
+        if step % NEW_PROXY_INTERVAL == 0:
+            # create new proxy
+            pass
+
+        # New clients get added (censor agent with chance of censoring agent)
+        if step % NEW_USER_RATE_INTERVAL == 0:
+            # create a new client (censor agent with the specific chance)
+            pass
 
         pass
 
