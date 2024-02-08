5504195dfcb9a2502b56005cc7ba6b3e590767fc
sinku <sininoir@gmail.com>
2024-02-07 19:36:05 -0500

    Add the first working proxy assignment

--- controller/scripts/simulation_utils.py
+++ controller/scripts/simulation_utils.py
@@ -2,10 +2,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.response import Response
 from rest_framework import status
 from assignments.services.startup import id_to_nums
-from scripts.config_basic import CENSORED_REGION_SIZE
 from random import randint
 from time import time
 from assignments.models import *
+from scripts.config_basic import CENSORED_REGION_SIZE, MAX_PROXY_CAPACITY, CENSOR_UTILIZATION_RATIO, CLIENT_UTILITY_THRESHOLD
+from scripts.deferred_acceptance import get_matched_clients
+
 
 def calcualte_distance(point_1, point_2):
     return ((point_1[0] - point_2[0])**2 + (point_1[1] - point_2[1])**2)**0.5
@@ -14,69 +16,105 @@ def get_normalized_distance(point_1, point_2):
     return calcualte_distance(point_1, point_2) / CENSORED_REGION_SIZE
 
 
-def request_new_proxy(user_ip):
-    try:
-        client = Client.objects.get(ip=user_ip)
-    except ObjectDoesNotExist:
-        client = Client.objects.create(ip=user_ip, user_agent="LemonadeOS device")
-        # O1: provide with random proxy and done. discuss this
-        # active_proxies = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all()
-        # chosen_proxy = active_proxies[randint(0,len(active_proxies) - 1)]
+def get_client_proxy_utilization(client, client_assignments, right_now):
+    proxy_checker = {}
+    clients_proxy_utilization = 0
+    for assignment in client_assignments:
+        if proxy_checker.get(assignment.proxy.id, None):
+            continue
+        if assignment.proxy.is_blocked == True:
+            clients_proxy_utilization += assignment.proxy.blocked_at - assignment.assignment_time
+        else:
+            clients_proxy_utilization += right_now - assignment.assignment_time
+        proxy_checker[assignment.proxy.id] = True
+    if client.is_censor_agent:
+        clients_proxy_utilization = clients_proxy_utilization * CENSOR_UTILIZATION_RATIO
+    
+    return clients_proxy_utilization
 
-        # O2: provide with random proxy and done. discuss this
-        chosen_proxy = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all().first()
 
-        Assignment.objects.create(proxy=chosen_proxy, client=client)
+def request_new_proxy_new_client(client):
+    chosen_proxy = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all().first()
+
+    Assignment.objects.create(proxy=chosen_proxy, client=client)
+    if Assignment.objects.filter(proxy=chosen_proxy, client=client).count() == 1:
         chosen_proxy.capacity -= 1
         chosen_proxy.save()
-        return chosen_proxy
+    return chosen_proxy
 
-    # ################ Enem19 implementation ################
+def request_new_proxy(proposing_clients, right_now:int):
+    client_prefrences = {}
+    proxy_prefrences = {}
+    proxy_capacities = {}
+    flagged_clients = []
 
     active_proxies = Proxy.objects.filter(is_blocked=False, is_active=True, capacity__gt=0).all()
-    client_assignments = Assignment.objects.filter(client=client)
-    known_blocked_proxies_for_client = client_assignments.values('proxy').filter(is_blocked=True)
-    
-    blocked_proxy_usage = 0
-    for proxy in known_blocked_proxies_for_client:
-        if not ProxyReport.objects.filter(proxy=proxy).values('clients').contains(client):
-            blocked_proxy_usage += 1
 
-    number_of_blocked_proxies_that_a_user_knows = known_blocked_proxies_for_client.count()
-    number_of_requests_for_new_proxies = client_assignments.count()
-    proxy_utilization = ProxyReport.objects.filter(connected_clients__value=client).count()
-
-    utility_values_for_client = []
-    alpha1, alpha2, alpha3, alpha4, alpha5 = 1,1,1,1,1
-    some_cap_value = 50
     for proxy in active_proxies:
-        distance = get_normalized_distance((proxy.latitude, proxy.longitude), (client.latitude, client.longitude))
-        proxy_utility = alpha1 * min(proxy_utilization, some_cap_value) \
-                        - alpha2 * number_of_requests_for_new_proxies \
-                        - alpha3 * blocked_proxy_usage \
-                        - alpha4 * number_of_blocked_proxies_that_a_user_knows \
-                        - alpha5 * distance
-        utility_values_for_client.append(proxy_utility)
+        utility_values_for_clients = {}
+        alpha1, alpha2, alpha3, alpha4, alpha5 = 1 ,50, 2, 10, 10
+        some_cap_value = 50 * 24
+        for client in proposing_clients:
+            if client.flagged == True:
+                continue
+            # ################ Enem19 implementation ################
+            client_assignments = Assignment.objects.filter(client=client).order_by('created_at')
+            assigned_proxies_list = client_assignments.values_list('proxy', flat=True).distinct()
+            known_blocked_proxies_for_client = Proxy.objects.filter(id__in=assigned_proxies_list, is_blocked=True)
+            known_proxies_for_client = Proxy.objects.filter(id__in=assigned_proxies_list)
+            
+            blocked_proxy_usage = 0
+            for assignment in client_assignments:
+                if assignment.proxy.is_blocked == True:
+                    blocked_proxy_usage += assignment.proxy.blocked_at - assignment.assignment_time
+
+            number_of_blocked_proxies_that_a_user_knows = known_blocked_proxies_for_client.count()
+            number_of_requests_for_new_proxies = client_assignments.count()
     
-    utility_values_for_proxies = []
-    beta1, beta2, beta3, beta4 = 1,1,1,1
-    for proxy in active_proxies:
-        distance = get_normalized_distance((proxy.latitude, proxy.longitude), (client.latitude, client.longitude))
-        number_of_connected_clients = ProxyReport.objects.filter(proxy=proxy).last().connected_clients.count()
-        number_of_clients_who_know_the_proxy = Assignment.objects.filter(proxy=proxy).values('client').distinct().count()
-        total_utilization_of_proxy_for_users = 0
-        client_utility =  beta1 * number_of_clients_who_know_the_proxy \
-                        + beta2 * number_of_connected_clients \
-                        + beta3 * total_utilization_of_proxy_for_users \
-                        - beta4 * distance
-        utility_values_for_proxies.append(client_utility)
+            clients_proxy_utilization = get_client_proxy_utilization(client, client_assignments, right_now)
+            distance = get_normalized_distance((proxy.latitude, proxy.longitude), (client.latitude, client.longitude))
+            client_utility = alpha1 * min(clients_proxy_utilization, some_cap_value) \
+                            - alpha2 * number_of_requests_for_new_proxies \
+                            - alpha3 * blocked_proxy_usage \
+                            - alpha4 * number_of_blocked_proxies_that_a_user_knows \
+                            - alpha5 * distance
+            
+            if client_utility < CLIENT_UTILITY_THRESHOLD:
+                client.flagged = True
+                client.save()
+                flagged_clients.append(client)
+            utility_values_for_clients[client.ip] = client_utility
+        proxy_prefrences[proxy.ip] = list(reversed(sorted(utility_values_for_clients, key=lambda k: utility_values_for_clients[k])))
+        proxy_capacities[proxy.ip] = proxy.capacity
 
-    mults = []
-    for i in range(len(utility_values_for_client)):
-        mults.append(utility_values_for_client[i] * utility_values_for_proxies[i])
+    for client in proposing_clients:
+        if client.flagged == True:
+                continue
+        # ################ Enem19 implementation ################
+        utility_values_for_proxies = {}
+        beta1, beta2, beta3, beta4 = 1,1,1,1
+        for proxy in active_proxies:
+            distance = get_normalized_distance((proxy.latitude, proxy.longitude), (client.latitude, client.longitude))
+            number_of_connected_clients = MAX_PROXY_CAPACITY - proxy.capacity
+            number_of_clients_who_know_the_proxy = Assignment.objects.filter(proxy=proxy).values_list('client', flat=True).distinct().count()
+            total_utilization_of_proxy_for_users = 0
+            proxy_utility =  beta1 * number_of_clients_who_know_the_proxy \
+                            + beta2 * number_of_connected_clients \
+                            + beta3 * total_utilization_of_proxy_for_users \
+                            - beta4 * distance
+            utility_values_for_proxies[proxy.ip] = proxy_utility
+        client_prefrences[client.ip] = list(reversed(sorted(utility_values_for_proxies, key=lambda k: utility_values_for_proxies[k])))
+        
+    matches = get_matched_clients(client_prefrences, proxy_prefrences, proxy_capacities)
 
-    chosen_proxy = active_proxies[mults.index(max(mults))]
-    Assignment.objects.create(proxy=chosen_proxy, client=client)
-    chosen_proxy.capacity -= 1
-    chosen_proxy.save()
-    return chosen_proxy
+    for proxy_id in matches.keys():
+        proxy = Proxy.objects.get(ip=proxy_id)
+        clients_accepted = matches[proxy_id]
+        for client_ip in clients_accepted:
+            client = Client.objects.get(ip=client_ip)
+            Assignment.objects.create(proxy=proxy, client=client, assignment_time=right_now)
+            if Assignment.objects.filter(proxy=proxy, client=client).count() == 1:
+                    proxy.capacity -= 1
+                    proxy.save()
+
+    return matches, flagged_clients
